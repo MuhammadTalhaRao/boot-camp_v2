a3c906b653a0ae4993b5d9c5b6ed49d95d75941d
Muhammad Talha <31736957+Dervaish19@users.noreply.github.com>
2020-08-26 16:21:09 +0500

    messing with jsx

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Cool_name from './ma_jsx';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Cool_name />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/ma_jsx.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+function Cool_name(){
+    const f_name = 'Talha';
+    const l_name = 'Khan';
+
+    return(
+        <h1>Ma Name is {l_name}, {f_name+l_name}</h1>
+    );
+}
+
+export default Cool_name;
