a832613ab0031343f4c472efe5eec8fc4bdf7eb2
Muhammad Talha <31736957+Dervaish19@users.noreply.github.com>
2020-08-27 20:06:01 +0500

    New counter using reducer

--- src/Parent.js
+++ src/Parent.js
@@ -1,15 +1,17 @@
 import React, { useState, useContext } from 'react';
 import Child1 from './child1';
 import Counter_Hook from './contextHook';
+import Child2 from './Child2';
 
 function Parent(){
-    let p_counter = useState(0);
-
+    let p_counter1 = useState(0);
+    
     return(
-        <Counter_Hook.Provider value = {p_counter} >
+        <Counter_Hook.Provider value = {p_counter1} >
         <div>
             <h1>This is Parent</h1>
             <Child1 />
+            <Child2 />
         </div>
         </Counter_Hook.Provider>
     );
--- src/child2.js
+++ src/child2.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useReducer, useContext } from 'react';
 import Child1 from './child1';
+import CounterReducer from './counterReducer';
+import Counter_Hook from './contextHook';
 
 function Child2(){
-    
+    let [state, dispatcher] = useReducer(CounterReducer, 0);
+    let counter = useContext(Counter_Hook);
+
+    return(
+        <div>
+            <h1>Counter: {state} </h1>
+            <button className = 'ClassicButton' onClick = {()=>dispatcher('INCREMENT')} >++</button>
+            <button className = 'ClassicButton' onClick = {()=>dispatcher('DECREMENT') } >--</button>
+        </div>
+    );
 }
 
 export default Child2;
