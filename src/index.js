8a4b326f7cf9a28f2ec3635093fcb64cb1eceab5
Muhammad Talha <31736957+Dervaish19@users.noreply.github.com>
2020-08-27 20:48:12 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Parent from './parent.js';
+import Parent from './Parent.js';
 
 ReactDOM.render(
   <React.StrictMode>
